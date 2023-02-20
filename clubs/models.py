a0d93d94dba6f0acb54be912cf399425fa6248ec
Joise MP <joise1124@gmail.com>
2023-02-20 12:31:11 +0530

    Created club delete view

--- clubs/models.py
+++ clubs/models.py
@@ -4,6 +4,6 @@ from django.db import models
 class ClubProfile(models.Model):
     name = models.CharField(max_length=250)
     description = models.TextField()
-    
+
     def __str__(self):
         return str(self.name)
--- clubs/urls.py
+++ clubs/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path("create", views.club_create_view, name="club-create"),
     path("<id>", views.club_detail_view, name="club-detail"),
     path("<id>/edit", views.club_edit_view, name="club-edit"),
+    path("<id>/delete", views.club_delete_view, name="club-delete"),
 ]
