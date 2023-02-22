e1494ac36eb8b6d8f49b07b4c665e9967ff84c33
Joise MP <joise1124@gmail.com>
2023-02-22 13:06:27 +0530

    Created Mixins
    - Mixin for org only access and check user type and redirect
    - Modified profile url from profile to org-profile
    - Modified ProfilePageView to OrgProfilePageView
    - Created Landing page view in views.py in accounts app
    - Removed Landing page view from main urls.py
    - Changed LoginView redirect to landing page
    - Used new Landing page view in main urls.py
    - Added login link in base.html file

--- main/urls.py
+++ main/urls.py
@@ -2,15 +2,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
+from accounts.views import LandingPageView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='landing_page.html'), name='landing-page'),
+    path('', LandingPageView.as_view(), name='landing-page'),
     path('accounts/', include('accounts.urls', namespace='accounts')),
     path('clubs/', include('clubs.urls', namespace='clubs')),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
