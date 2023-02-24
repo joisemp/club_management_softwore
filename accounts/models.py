f7589e1ec828a68b16124f4760ec740d55a01be9
Joise MP <joise1124@gmail.com>
2023-02-25 00:20:58 +0530

    Feature Added : Create a user with student profile
    - Created Student Profile Model
    - Registered Student Profile Model in admin.py
    - Created StudentProfileForm
    - Added uitils.py file and created a generate_password function in it
    - Created new url for student profile creation
    - Created StudentRegisterView
    - If the form is valid it will create a user and student profile with the given information
    - On success redirect user to landing page
    - Created register_student.html file

--- accounts/models.py
+++ accounts/models.py
@@ -9,6 +9,7 @@ class User(AbstractUser):
     username = None
     email = models.EmailField(_('email address'), unique=True)
     is_org = models.BooleanField(_('is organisation'), default=False)
+    is_student = models.BooleanField(_('is student'), default=False)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
@@ -19,3 +20,17 @@ class User(AbstractUser):
 class OrgProfile(models.Model):
     user = models.OneToOneField(User, models.CASCADE)
     name = models.CharField(max_length=300)
+    
+    def __str__(self):
+        return str(self.name)
+    
+
+class StudentProfile(models.Model):
+    user = models.OneToOneField(User, models.CASCADE)
+    org = models.ForeignKey(OrgProfile, models.CASCADE)
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+    email = models.EmailField(unique=True)
+    
+    def __str__(self):
+        return str(self.first_name) + " " + str(self.last_name)
--- accounts/urls.py
+++ accounts/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
     path('register/', views.UserRegisterView.as_view(), name='register'),
+    path('register-student/', views.StudentRegisterView.as_view(), name='register-student'),
     path('activate/<uidb64>/<token>/', views.activate, name='activate'),
     path('change-password/', login_required(views.ChangePasswordView.as_view()),
          name='change-password'),
